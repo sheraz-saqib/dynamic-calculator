d632742bf293d08de07c085954f32bf41979be90
sheraz-saqib <bhanakop@gmail.com>
2023-01-20 15:30:09 +0500

    calculator

---
+++ app.js
@@ -0,0 +1,18 @@
+
+function cl(){
+    var result = document.getElementById("di")
+    result.value=""
+}
+function getNumber(nam){
+    var result=document.getElementById("di")
+   result.value += nam
+}
+
+function result(){
+    var result = document.getElementById("di")
+    result.value= eval(result.value)
+}
+function del(i){
+    var result = document.getElementById("di")
+    result.value ='' 
+}
